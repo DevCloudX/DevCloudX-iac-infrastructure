3a3e3d254cd62858297864e3eb6bb5e7581ddcb0
Abhishek-X-Rana_qd <Abhishek.X.Rana@questdiagnostics.com>
2025-05-06 09:43:34 +0000

    added

--- variables.tf
+++ variables.tf
@@ -1,4 +1,19 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "The AMI ID to use for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of EC2 instance"
+  type        = string
+}
+
 variable "instance_name" {
-  description = "Name for the EC2 instance"
+  description = "The name to tag the EC2 instance"
   type        = string
 }
