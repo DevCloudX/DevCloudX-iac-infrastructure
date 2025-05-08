dfd29f7b248df0d1c51c4c9449cedcd8f240cd27
Abhishek rana <me.abhishekrana0317@gmail.com>
2025-05-08 10:42:27 +0530

    added aws-subnet

---
+++ aws-subnet/main.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_subnet" "custom_subnet" {
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.cidr_block
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = var.subnet_name
+  }
+}
+
+variable "aws_access_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "aws_secret_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "subnet_name" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "cidr_block" {
+  type = string
+}
+
+variable "availability_zone" {
+  type = string
+}
