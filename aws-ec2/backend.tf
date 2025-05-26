82d53d46628a0f66b60724ecf48206dcf7164bed
Abhishek rana <me.abhishekrana0317@gmail.com>
2025-05-26 21:10:35 +0530

    added

--- aws-ec2/backend.tf
+++ aws-ec2/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "devcloudx-tf-aws-s3-bucket"
-    key    = "ec2/terraform.tfstate"
+    key    = "aws-ec2/terraform.tfstate"
     region = "us-east-1"
     encrypt = true
   }
